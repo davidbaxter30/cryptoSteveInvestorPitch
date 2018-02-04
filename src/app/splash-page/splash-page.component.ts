700a545b41318f8c8e46e854646a216fe390942a
David Baxter <baxter30@gmail.com>
2018-02-03 19:55:47 -0700

    switching work stations

--- src/app/splash-page/splash-page.component.ts
+++ src/app/splash-page/splash-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { timeout } from 'q';
 
 @Component({
   selector: 'app-splash-page',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SplashPageComponent implements OnInit {
 
+  line: boolean[];
+
   constructor() { }
 
   ngOnInit() {
+    this.line = new Array(12).fill(true, 0, 12);
+    this.revealConsoleReadout();
+  }
+
+  revealConsoleReadout(): void {
+    this.line.forEach((item: boolean, index: number) => {
+      setTimeout(
+        () => {
+          // uncomment to avoid previous line showing up after this one. 
+          // if (this.line[index -1]) this.line[index -1] = false;
+          this.line[index] = false
+        }, 
+        (Math.random() * 1000) + index * 500
+      );
+    })
+  }
+
+  hideLine(lnNumber: number): boolean {
+    return this.line[lnNumber];
   }
 
 }
